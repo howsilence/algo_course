4f493dd8ffa9912120b10e680a17f0625aa76cd6
howsilence <statton0104@gmail.com>
2022-05-26 11:33:17 -0400

    swapped off Array from to split

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,21 +8,26 @@
 
 function reverse(str) {
     //create an array from our params
-    let arr = Array.from(str)
-    console.log(arr, "arr")
+    // let arr = Array.from(str)
+    // console.log(arr, "arr")
 
-    //reverse the array
-    let reversed = arr.reverse();
-    console.log(reversed, "reversed")
+    // //creating the array can also be done by
+    // let arr = str.split('');
+    // console.log(arr, "split")
 
-    //return it to the string via join
-    let result = reversed.join('');
-    console.log(result, "result");
-    return result
+    // //reverse the array
+    // let reversed = arr.reverse();
+    // console.log(reversed, "reversed")
+
+    // //return it to the string via join
+    // let result = reversed.join('');
+    // console.log(result, "result");
+    // return result
 
     //one line
     // console.log(Array.from(str).reverse().join(''));
     // return Array.from(str).reverse().join('');
+    return str.split('').reverse().join('')
 };
 
 module.exports = reverse;
