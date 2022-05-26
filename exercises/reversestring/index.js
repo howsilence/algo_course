1f36db2e4b4ca393898d735cc940f12f8f2851ff
howsilence <statton0104@gmail.com>
2022-05-26 11:28:28 -0400

    first solution to reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //create an array from our params
+    let arr = Array.from(str)
+    console.log(arr, "arr")
+
+    //reverse the array
+    let reversed = arr.reverse();
+    console.log(reversed, "reversed")
+
+    //return it to the string via join
+    let result = reversed.join('');
+    console.log(result, "result");
+    return result
+
+    //one line
+    // console.log(Array.from(str).reverse().join(''));
+    // return Array.from(str).reverse().join('');
+};
 
 module.exports = reverse;
