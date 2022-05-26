ec24b72d4948d0df6a79c82c1536bf50b3cf94c5
howsilence <statton0104@gmail.com>
2022-05-26 11:46:57 -0400

    for of loop solution instead of reverse method

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,6 +7,23 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
+    //initialize an empty string
+    let reversed = '';
+
+    //use for of syntax to iterate
+    // for of is great when you need to iterate over every instance of an object but you must rever to a normal for loop if you need to iterate over every 3rd instance or every 5th etc.
+    // for a temporary variable (character (redefined every iteratin) of (for each character of string) do this(add character to the reversed variable) return the reversed string variable)
+    for (let character of str) {
+        console.log(reversed, "start loop")
+        reversed = character + reversed;
+        console.log(reversed, "exit loop")
+    }
+    return reversed;
+   
+};
+
+
+ //solution one
     //create an array from our params
     // let arr = Array.from(str)
     // console.log(arr, "arr")
@@ -27,7 +44,6 @@ function reverse(str) {
     //one line
     // console.log(Array.from(str).reverse().join(''));
     // return Array.from(str).reverse().join('');
-    return str.split('').reverse().join('')
-};
+    // return str.split('').reverse().join('')
 
 module.exports = reverse;
