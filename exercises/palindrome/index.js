7be1a7621b1581fed78ea48c60575da5297a9008
howsilence <statton0104@gmail.com>
2022-06-02 12:35:03 -0400

    palindrome and reverseint done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,42 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+   // take the str and split it into an array
+   // make a new array that is a copy but reversed
+   // pop each element of new array and compare
+//    let start = str.split('');
+//    let reverse = start.reverse();
 
+//    for (i = 0; i <= reverse.length; i++){
+//        let comparison = reverse.pop()
+//        let counter = start[0]++
+//         if (comparison !== counter){
+//             console.log(`${str} is not a palindome.`)
+//         }
+//    }
+ 
+    // solution one
+    // let reverse = str.split('').reverse().join();
+    // return str === reverse
+    
+    // solution two use array.every()
+    // every is map for comparisons, it checks each value in an array and compares, if comparison is success it returns true else it stops
+    // passes the character and the index of the array as args
+    // the return statement compares char to str[index of str.length - index -1]
+   return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    })
+
+
+
+
+}
 module.exports = palindrome;
+
+
+//for using debugger you need to call the function afterwards, ex:
+// function palindrome(poop){
+//     debugger;
+// }
+// then node inspect file.js
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,30 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//hits given: Math.sign() -- returns symbol of number +, - or 0
+// parseInt() checks string for numbers
+// toString() takes input and turns it into a string
+
+function reverseInt(n) {
+
+    //no need for complicated loops, this bit is constant so we convert n to string, split it into an array, reverse it then join it back together
+    const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+    // this if statement checks if n is negative, if so it multiplies the number by negative 1 which will make it negative and drop any unnecesarry 0's
+    if (n < 0){
+        return parseInt(reversed) * -1;
+    }
+    return parseInt(reversed);
+
+    // another more effecient way to write this if statement is:
+    // return parseInt(reversed) * Math.sign(n)
+    // this will multiply by 1 or -1 depending on if n is + or -
+
+}
+
 
 module.exports = reverseInt;
