b2ba83ded5d8acc4b9d21bb3cffbb90b93bf09ef
howsilence <statton0104@gmail.com>
2022-06-02 14:38:28 -0400

    fizzbuzz complete

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,33 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+        if (i % 3 !== 0 && i % 5 !== 0) {
+        console.log(i);
+        } else if (i % 3 === 0 && i % 5 !==0) {
+        console.log("fizz")
+        } else if (i % 3 !== 0 && i % 5 === 0){
+        console.log("buzz")
+        } else if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz");
+        }
+    };
+};
+
+// function fizzBuzz(n) {
+//     for(let i = 0; i <= n; i++){
+//         if (i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz");
+//         } else if (i % 5 === 0 && i % 3 !==0){
+//             console.log("Buzz");
+//         } else if (i % 3 === 0 && i % 5 !==0) {
+//             console.log("Fizz")
+//         } else {
+//             console.log(i)
+//         }
+//     }
+
+// };
 
 module.exports = fizzBuzz;
