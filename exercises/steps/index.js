32cef9b91e7d9dece5e549a18f7301feee7fa470
howsilence <statton0104@gmail.com>
2022-06-08 13:11:46 -0400

    steps complete, recursion solution included

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,83 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// think of this like a grid with a row variable and column variable
+//          ----> column
+//     |    '# _ _ '
+//    V     '# # _ '
+//   row    '# # # '
+// 
+    //from 0 to n (iterate through rows)
+    // create an empty string "stair"
+    // from 0 to n (iterate through columns)
+    // IF the current column is equal to or less than the current row
+    // add '#' to stair
+    // ELSE
+    // add a space to 'stair'
+    //console log 'stair'
+function steps(n, row = 0, stair = '') {
+    
+    //recursive solution from bootcamp
+    // start with base case to prevent infinite loop
+    if (row === n) {
+        return;
+    }
+
+    if(stair.length === n){
+        console.log(stair);
+        return steps(n, row +1);
+    }
+
+    if(stair.length <= row){
+        stair += '#';
+        
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+};
+
+// first pass
+// function steps(n) {
+//     //write a function that accepts a positive number
+//     if(n < 0){
+//         return n
+//     }
+
+//     //function console logs a step shape of N levels using the # sign
+//     // steps should have a space on the right hand side
+//     //i think i need to make an array and push to it then string it and log
+//     // furthermore i think the chunked problem will be used in this solution
+//     const index = 0;
+//     const counter = 1;
+//     const chunk = [];
+//     const space = ' ';
+//     const pound = '#';
+
+//     while(counter <= n) {
+//         let slice = array.slice(index, index + counter)
+//         chunk.push(slice)
+//         index += counter 
+//     }
+// }
+
+// second pass, solution
+// function steps(n) {
+    
+//     for(let row = 0; row < n; row++){
+//         let step = '';
+//         for(let col = 0; col < n; col++){
+//             if(col <= row){
+//                 step += '#';
+//             } else {
+//                 step += ' '
+//             }
+//         }
+//         console.log(step);
+//     }
+
+// };
+
 
 module.exports = steps;
